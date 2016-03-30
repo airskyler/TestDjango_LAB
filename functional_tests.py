fac70235f27bcdfbb1535983cf9a56e7a02af70a
jessy <mullenassociation@gmail.com>
2016-03-29 23:13:35 -0500

    Test book files

--- functional_tests.py
+++ functional_tests.py
@@ -1,9 +1,102 @@
 
 
 from selenium import webdriver # using webdriver to use a browser
+from selenium.webdriver.common.keys import Keys
+import unittest
 
-browser = webdriver.Firefox() # use Firefox as a brower
-browser.get('http://localhost:8000')
 
-assert 'Django' in browser.title
 
+class NewVisitorTest(unittest.TestCase):  #1
+
+    def setUp(self):  # use Firefox as a browser
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
+		
+
+    def tearDown(self):  # quit using browser
+        self.browser.quit()
+
+		
+		
+		# using find_element_by_id and find_element_by_tag_name to search through a HTML code
+		
+    def test_can_start_a_list_and_retrieve_it_later(self):  
+        # Edith has heard about a cool new online to-do app. She goes
+        # to check out its homepage
+        self.browser.get('http://localhost:8000')
+
+        self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+
+        # She is invited to enter a to-do item straight away
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(inputbox.get_attribute('placeholder'),'Enter a to-do item')
+
+        # She types "Buy peacock feathers" into a text box (Edith's hobby
+        # is tying fly-fishing lures)
+        inputbox.send_keys('Buy peacock feathers')
+
+        # When she hits enter, the page updates, and now the page lists
+        # "1: Buy peacock feathers" as an item in a to-do list table
+        inputbox.send_keys(Keys.ENTER)
+		
+		import time
+        # time.sleep(10)  # using to pause the test while executing
+        table = self.browser.find_element_by_id('id_list_table')
+
+		
+		
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(any(row.text == '1: Buy peacock feathers' for row in rows),"New to-do item did not appear in table")
+
+        # There is still a text box inviting her to add another item. She
+        # enters "Use peacock feathers to make a fly" (Edith is very
+        # methodical)
+        self.fail('Finish the test!')
+
+        # The page updates again, and now shows both items on her list
+        [...]
+	
+	
+	
+	
+	
+	
+	
+	
+# browser = webdriver.Firefox() # use Firefox as a brower
+# browser.get('http://localhost:8000')
+
+
+
+
+#assert 'Django' in browser.title
+
+
+# She notices the page title and header mention to-do lists
+# assert 'To-Do' in browser.title
+
+# She is invited to enter a to-do item straight away
+
+# She types "Buy peacock feathers" into a text box (Edith's hobby
+# is tying fly-fishing lures)
+
+# When she hits enter, the page updates, and now the page lists
+# "1: Buy peacock feathers" as an item in a to-do list
+
+# There is still a text box inviting her to add another item. She
+# enters "Use peacock feathers to make a fly" (Edith is very methodical)
+
+# The page updates again, and now shows both items on her list
+
+# Edith wonders whether the site will remember her list. Then she sees
+# that the site has generated a unique URL for her -- there is some
+# explanatory text to that effect.
+
+# She visits that URL - her to-do list is still there.
+
+# Satisfied, she goes back to sleep
+
+# browser.quit()
--- lists/tests.py
+++ lists/tests.py
@@ -1,8 +1,45 @@
-from django.test import TestCase
 
-
-class SmokeTest(TestCase):
-
-    def test_bad_maths(self):
-        self.assertEqual(1 + 1, 3)
 		
+from django.core.urlresolvers import resolve
+from django.test import TestCase
+from lists.views import home_page #use view function to display
+from django.http import HttpRequest
+from django.template.loader import render_to_string
+
+
+
+
+class HomePageTest(TestCase):
+
+    def test_root_url_resolves_to_home_page_view(self):
+        found = resolve('/')  # looking for "/" on the root site
+        self.assertEqual(found.func, home_page)
+		
+		
+    def test_home_page_returns_correct_html(self):
+	
+        request = HttpRequest()  # Creating a HttpRequest object
+
+        response = home_page(request)  # Pass the request to home_page view and get a response
+		
+		self.assertIn('A new list item', response.content.decode())
+        expected_html = render_to_string('home.html',{'new_item_text':  'A new list item'})
+
+        self.assertEqual(response.content.decode(), expected_html)
+		
+	
+	
+	
+	def test_home_page_can_save_a_POST_request(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = 'A new list item'
+
+        response = home_page(request)
+
+        self.assertIn('A new list item', response.content.decode())
+		
+		
+        #self.assertTrue(response.content.startswith(b'<html>'))  # looking for HTML tag
+        #self.assertIn(b'<title>To-Do lists</title>', response.content)  # Looking for a title tag with word "To-Do List"
+        #self.assertTrue(response.content.strip().endswith(b'</html>'))  
